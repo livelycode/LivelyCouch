967f218cc9fcfb0aea1331909c52df5ab1f2fa93
Mirko Kiefer <mail@mirkokiefer.com>
2011-02-11 13:03:01 +0100

    added log4js

---
+++ tests/resolve-bindings.js
@@ -0,0 +1,14 @@
+var vows = require('vows'),
+assert = require('assert');
+var myutils = require('../lib/myutils');
+
+vows.describe('resolve-bindings').addBatch({
+  'simple match':  {
+    topic: function() {
+
+    },
+    'is valid': function(err, config) {
+
+    }
+  }
+}).export(module);
