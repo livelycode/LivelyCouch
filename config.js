32c3404eaefddffa313d422a8dddb37af2e9e18f
Mirko Kiefer <mail@mirkokiefer.com>
2011-02-21 13:11:26 +0100

    changed 'events' to 'subscribers' as its clearer

--- config.js
+++ config.js
@@ -3,12 +3,12 @@ var client = couchdb.createClient(5984, '127.0.0.1', 'lively', 'lively');
 exports.couchdb = couchdb;
 exports.client = client;
 
-exports.eventsSource = [__dirname + '/events-source/'];
+exports.eventsSource = [__dirname + '/event-subscribers/'];
 exports.workersSource = [__dirname + '/workers-source/'];
 exports.workersDeployed = __dirname + '/workers-deployed/';
 exports.logPath = __dirname + '/logs/';
 
-exports.eventsDbName = 'lively_events';
+exports.eventsDbName = 'lively_subscribers';
 exports.workersDbName = 'lively_workers';
 exports.logsDbName = 'lively_logs';
 
@@ -17,4 +17,3 @@ exports.logToCouch = true;
 exports.eventsDb = client.db(exports.eventsDbName);
 exports.workersDb = client.db(exports.workersDbName);
 exports.logsDb = client.db(exports.logsDbName);
-
