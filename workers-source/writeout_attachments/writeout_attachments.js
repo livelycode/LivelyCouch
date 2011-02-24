addd35defe31562841b1235f9a596455af9942a9
Mirko Kiefer <mail@mirkokiefer.com>
2011-02-24 13:48:34 +0100

    deleted redundant write_handler worker, created writeout_attachment worker with documentation

--- workers-source/writeout_attachments/writeout_attachments.js
+++ workers-source/writeout_attachments/writeout_attachments.js
@@ -1,4 +1,3 @@
-
 var http = require('http');
 var url = require('url');
 var sys = require('sys');
@@ -6,34 +5,28 @@ var fs = require('fs');
 var workerLib = require('../../lib/workerlib');
 var couchdb = workerLib.couchdb;
 
-var name = 'writefiles';
+var name = 'writeout_attachments';
 workerLib.initialize(name, function() {
   var eventStream = workerLib.openEventStream();
-  
+
   eventStream.on('event', function(event) {
     execute(event);
   });
-  
   eventStream.on('end', function() {
     process.exit(0);
   });
 });
-
 var execute = function(event) {
   var client = workerLib.client;
-  client.request('/_config/lively', function(err, response) {
-    var handlerPath = response.handler_path;
-    var docId = event.parameters.docid;
-    var dbName = 'lively_handlers';
-    writeOutAttachments(client,dbName, docId, handlerPath, function() {
-      workerLib.emitLivelyEvent('attachments_written', {docid:docId, dbname:dbName, folderpath:handlerPath})
-    });
+  var folder = event.parameters.folderpath;
+  var docId = event.parameters.docid;
+  var dbName = event.parameters.db;
+  writeOutAttachments(client,dbName, docId, folder, function() {
+    workerLib.emitLivelyEvent('attachments_written', {docid:docId, db:dbName, folderpath:folder})
   });
 }
-
 var writeOutAttachments = function(client, db, docId, folderPath, callback) {
   var db = client.db(db);
-
   db.getDoc(docId, function (error, doc) {
     if(doc) {
       if (doc._attachments) {
@@ -46,19 +39,14 @@ var writeOutAttachments = function(client, db, docId, folderPath, callback) {
           var writeOutAttachment = function(fileNames) {
             var currFileName = fileNames[fileNames.length-1];
             db.getAttachment(docId, currFileName, function(err, attachment) {
-              try {
-                fs.statSync(folderPath + doc._id + '/');
-              } catch(e) {
-                fs.mkdirSync(folderPath + doc._id + '/', 0777);
-              }
-              fs.writeFileSync(folderPath + doc._id + '/' + currFileName, attachment, encoding='utf8');
+              fs.writeFileSync(folderPath + '/' + currFileName, attachment, encoding='utf8');
               fileNames.pop();
               if(fileNames.length > 0) {
-                writeOutAttachment(fileNames);          
+                writeOutAttachment(fileNames);
               } else {
                 callback(attachmentNames)
               }
-            });  
+            });
           }
           writeOutAttachment(attachmentNames);
         }
