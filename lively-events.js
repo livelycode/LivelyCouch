0993c6b5324c22f08901632d1b036dbe11ac44db
Mirko Kiefer <mail@mirkokiefer.com>
2011-02-24 13:01:55 +0100

    added file logger

--- lively-events.js
+++ lively-events.js
@@ -1,53 +1,52 @@
-var workerLib = require('./lib/workerlib');
-var deployment = require('./lib/deployment').init({workerLib: workerLib});
-var workerManagement = require('./lib/worker_management').init({
-  workerLib: workerLib,
-  deployment: deployment
-});
-var subscriptionHandling = require('./lib/subscription_handling').init({
-  workerLib: workerLib,
-  workerManagement: workerManagement
-});
 var EventEmitter = require('events').EventEmitter;
 var statusEmitter = new EventEmitter();
-
-var myutils = require('./lib/myutils');
-
-var openStdin = function() {
-  var stdin = process.openStdin();
-  stdin.on('data', function(d) {
-    server.listen(parseInt(JSON.parse(d)));
+var workerLib = require('./lib/workerlib');
+workerLib.initialize('lively_events', function() {
+  var deployment = require('./lib/deployment').init({workerLib: workerLib});
+  var workerManagement = require('./lib/worker_management').init({
+    workerLib: workerLib,
+    deployment: deployment
   });
-  stdin.on('end', function() {
-    workerLib.emitLivelyEvent(stopEvent);
-    console.log('lively-events.js exits');
-    process.exit(0);
+  var subscriptionHandling = require('./lib/subscription_handling').init({
+    workerLib: workerLib,
+    workerManagement: workerManagement
   });
-}
-var startup = function(startupCb) {
-  openStdin();
-  myutils.doLinear([
-  function(cb) {
-    workerLib.initialize('lively_events', cb)
-  },
-  function(cb) {
-    deployment.checkAndDeploy(cb)
-  },
-  function(cb) {
-    subscriptionHandling.createEventsChangeListener(cb)
-  },
-  function(cb) {
-    subscriptionHandling.launchEventSystem(cb)
-  },
-  function(cb) {
-    deployment.createWorkerChangeListener();
-    cb();
+
+  var myutils = require('./lib/myutils');
+
+  var openStdin = function() {
+    var stdin = process.openStdin();
+    stdin.on('data', function(d) {
+      server.listen(parseInt(JSON.parse(d)));
+    });
+    stdin.on('end', function() {
+      workerLib.emitLivelyEvent('stopped');
+      console.log('lively-events.js exits');
+      process.exit(0);
+    });
   }
-  ], function() {
-    startupCb();
+  var startup = function(startupCb) {
+    openStdin();
+    myutils.doLinear([
+    function(cb) {
+      deployment.checkAndDeploy(cb)
+    },
+    function(cb) {
+      subscriptionHandling.createEventsChangeListener(cb)
+    },
+    function(cb) {
+      subscriptionHandling.launchEventSystem(cb)
+    },
+    function(cb) {
+      deployment.createWorkerChangeListener();
+      cb();
+    }
+    ], function() {
+      startupCb();
+    });
+  }
+  startup( function() {
+    statusEmitter.emit('started');
   });
-}
-startup( function() {
-  statusEmitter.emit('started');
 });
 exports.statusEmitter = statusEmitter;
